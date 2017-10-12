b3cd47d5eeb71027d7a84ab85bd46579f0946d28
mikehajj <mike.hajj@gmail.com>
2017-10-12 16:45:08 +0300

    added new ckeditor input plugin

--- engine/lib/form/form.js
+++ engine/lib/form/form.js
@@ -281,7 +281,7 @@ function buildForm(context, modal, configuration, cb) {
 			};
 		}
 	}
-
+	
 	if (configuration.data) {
 		for (var i = 0; i < context.form.entries.length; i++) {
 			if (context.form.entries[i].type === 'group') {
@@ -630,6 +630,93 @@ soajsApp.directive('fileModelMulti', ['$parse', function ($parse) {
 					modelSetter(scope, element[0].files);
 				});
 			});
+		}
+	};
+}]);
+
+soajsApp.directive('ckeditor', ['$timeout', '$q', function ($timeout, $q) {
+	return {
+		restrict: 'AC',
+		require: ['ngModel', '^?form'],
+		scope: false,
+		link: function (scope, element, attrs, ctrls) {
+			var ngModel = ctrls[0];
+			var form    = ctrls[1] || null;
+			var EMPTY_HTML = '<p></p>',
+				isTextarea = element[0].tagName.toLowerCase() == 'textarea',
+				data = [],
+				isReady = false;
+			
+			if (!isTextarea) {
+				element.attr('contenteditable', true);
+			}
+			
+			var onLoad = function () {
+				var options = {};
+				options = angular.extend(options, scope[attrs.ckeditor]);
+				
+				var instance = (isTextarea) ? CKEDITOR.replace(element[0], options) : CKEDITOR.inline(element[0], options),
+					configLoaderDef = $q.defer();
+				
+				element.bind('$destroy', function () {
+					instance.destroy(
+						false //If the instance is replacing a DOM element, this parameter indicates whether or not to update the element with the instance contents.
+					);
+				});
+				var setModelData = function(setPristine) {
+					var data = instance.getData();
+					if (data == '') {
+						data = null;
+					}
+					$timeout(function () { // for key up event
+						(setPristine !== true || data != ngModel.$viewValue) && ngModel.$setViewValue(data);
+						(setPristine === true && form) && form.$setPristine();
+					}, 0);
+				}, onUpdateModelData = function(setPristine) {
+					if (!data.length) { return; }
+					
+					
+					var item = data.pop() || EMPTY_HTML;
+					isReady = false;
+					instance.setData(item, function () {
+						setModelData(setPristine);
+						isReady = true;
+					});
+				};
+				
+				//instance.on('pasteState',   setModelData);
+				instance.on('change',       setModelData);
+				instance.on('blur',         setModelData);
+				//instance.on('key',          setModelData); // for source view
+				
+				instance.on('instanceReady', function() {
+					scope.$broadcast("ckeditor.ready");
+					scope.$apply(function() {
+						onUpdateModelData(true);
+					});
+					
+					instance.document.on("keyup", setModelData);
+				});
+				instance.on('customConfigLoaded', function() {
+					configLoaderDef.resolve();
+				});
+				
+				ngModel.$render = function() {
+					data.push(ngModel.$viewValue);
+					if (isReady) {
+						onUpdateModelData();
+					}
+				};
+			};
+			
+			if (CKEDITOR.status == 'loaded') {
+				loaded = true;
+			}
+			if (loaded) {
+				onLoad();
+			} else {
+				$defer.promise.then(onLoad);
+			}
 		}
 	};
 }]);
